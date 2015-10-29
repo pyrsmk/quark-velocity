d00883d031e1cf9b3d63f39030dbc917b17f1038
pyrsmk <dev@dreamysource.fr>
2015-10-29 17:14:12 +0100

    Update build

--- src/quark-velocity.js
+++ src/quark-velocity.js
@@ -1,4 +1,4 @@
-/*! quark-velocity 0.1.0 (https://github.com/pyrsmk/quark-velocity) */
+/*! quark-velocity 0.1.1 (https://github.com/pyrsmk/quark-velocity) */
 
 quark.$._nodeMethods.animate = function(properties, options) {
 	Velocity(this.node, properties, options);
